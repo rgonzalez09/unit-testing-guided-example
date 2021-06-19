074aa49345522bfaaa0b288150ad97da2f052046
Rodrigo <rgonzalez09@gmail.com>
2021-06-19 16:11:32 -0400

    tests passed for sum subtract and divide

--- calculator.js
+++ calculator.js
@@ -1,17 +1,43 @@
 function sum(a, b) {
-  return;
+  if (!a && !b){
+    return 0
+  }
+  else if(!a){
+    return b
+  }
+  else if(!b){
+    return a
+  }
+  return a + b;
+  
 }
 
 function subtract(a, b) {
-  return;
+  if (!a && !b){
+    return 0
+  }
+  else if(!a){
+    return b
+  }
+  else if(!b){
+    return a
+  }
+  return a - b;
+  
 }
 
 function divide(a, b) {
-  return;
+  if(a===0){
+    return error
+  }
+  else if(b===0){
+    return error
+  }
+return a / b;
 }
 
 function multiply(a, b) {
-  return a * b;
+  return a*b;
 }
 
 // The following is required to make unit tests work. Please ignore it.
